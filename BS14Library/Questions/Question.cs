209df05f156190c9b5a874244235ea8ae8b351e4
Noobi - Miles <ltb350@hotmail.de>
2017-02-10 13:24:12 +0100

    Added 3 more Projects and propper Documentation to the Library

--- BS14Library/Questions/Question.cs
+++ BS14Library/Questions/Question.cs
@@ -11,11 +11,21 @@ namespace BS14Library.Questions
     /// </summary>
     class Question
     {
+        #region [ Blaupause ]
+        #endregion
+
+
+        #region [ Constructors ]
+        
         public Question(int questionNo, QuestionType type)
         {
             this.QuestionNumber = questionNo;
             this.QuestionType = type;
         }
+
+        #endregion
+
+        #region [ Props ]
 
         /// <summary>
         /// The Number of the Question
@@ -26,5 +36,37 @@ namespace BS14Library.Questions
         /// The Type of the Question
         /// </summary>
         public QuestionType QuestionType { get; private set; }
+
+        #endregion
+
+        #region [ Methods ]
+
+        /// <summary>
+        /// Converts this class to an XML-Store Format
+        /// </summary>
+        /// <returns></returns>
+        public string ToXML()
+        {
+            throw new NotImplementedException();
+        }
+
+        #region [ static Methods ]
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="isPath"></param>
+        /// <returns></returns>
+        public Question FromXML(string filePath, bool isPath = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+
+        #endregion
+
     }
 }
